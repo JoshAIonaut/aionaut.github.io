6b5683a265f46bd00f9fc96303adbcce98c469d7
Josh <buch.joshua@gmail.com>
2025-10-29 09:46:29 +0100

    Codex Test

--- aionaut-web/next.config.js
+++ aionaut-web/next.config.js
@@ -1,8 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  swcMinify: true
+  swcMinify: true,
+  // Statischer Export für GitHub Pages
+  output: 'export'
 };
 
 module.exports = nextConfig;
-
